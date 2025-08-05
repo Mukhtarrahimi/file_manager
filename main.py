ddd75fe7968db6d60021de857b5fc629a370bc08
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-05 09:50:21 +0430

    open_file function.

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ def file_open_box():
 def directory_open_box():
     path = tk.filedialog.askdirectory()
     return path
+
+# open_file fuction
+
+def open_file():
+    path = file_open_box()
+    try:
+        os.startfile(path)
+    except:
+        tk.messagebox.showinfo('Error!', 'file not found.')
+        
+open_file()
